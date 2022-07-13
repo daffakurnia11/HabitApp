b69e08f4cf5cec02467701ded31a73ff447349c1
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2022-07-13 18:06:29 +0700

    Creating UI Testing when user taps Add Habit button

--- app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
@@ -1,4 +1,27 @@
-package com.dicoding.habitapp.ui.list
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.add.AddHabitActivity
+import com.dicoding.habitapp.ui.list.HabitListActivity
+import org.junit.Before
+import org.junit.Test
 
 //TODO 16 : Write UI test to validate when user tap Add Habit (+), the AddHabitActivity displayed
-class HabitActivityTest
+class HabitActivityTest {
+    @Before
+    fun setUp(){
+        ActivityScenario.launch(HabitListActivity::class.java)
+    }
+
+    @Test
+    fun showAddTaskActivity(){
+        Intents.init()
+        Espresso.onView(ViewMatchers.withId(R.id.fab)).perform(ViewActions.click())
+        Intents.intended(IntentMatchers.hasComponent(AddHabitActivity::class.java.name))
+        Intents.release()
+    }
+}
